5087382f7f35fe175a864832517881957984a716
aditikantharia <aditikantharia@gmail.com>
2026-03-03 13:14:48 +0530

    this file add

--- This/this.js
+++ This/this.js
@@ -10,3 +10,132 @@
 
 
 // this keyborad context is determined by how a function is called (that is the reason why this keyword is called dynamic scope) (this ni value this nu use kaya context ma chhe te par depend kare chhe)
+
+// globl text
+console.log("global",this); // window
+
+// function es5 context
+function abc(){
+    console.log("function",this);
+}
+abc();// window
+
+//function es6 context
+let bcd = () =>{
+    console.log("fnc ES6",this);
+};
+bcd(); // window
+
+//method context
+// method --> function in object
+
+
+// 1. method --> fnc es5
+
+let a = {
+  name: "test",
+  age: 25,
+  write: function () {
+    // method --> ek function je koi object ni under hoy tene
+    // method kevay. // this --> give object
+    // if you use arrow function then this key give you windows
+    // object because arrow function does not have its own this
+    // keyword it takes this value from its parent scope
+
+    // if you create one more function inside method then this
+    // looses their value --> soluation --> you have to create a arrow function
+    console.log("method fnc es5:", this);
+  },
+};
+ a.write();
+
+// 2. method --> fnc es6
+let b = {
+  name: "test",
+  age: 25,
+  write: () => {
+    console.log("method --> fnc es6", this);
+  },
+};
+
+b.write(); // window
+
+
+// 3. method --> fnc es5 ==> fnc es5
+let c = {
+  name: "test",
+  age: 25,
+  write: function () {
+    function abc() {
+      console.log("method --> fnc es6 into fnc es5", this);
+    }
+    abc();
+  },
+};
+
+c.write(); // window
+
+//4. method --> fnc es5 ==> fnc es6 (arrow function)
+let d ={
+    name:"aditi",
+    age: 20,
+    write: function(){
+        let bcd = () => {
+            console.log("method --> es5 into es6", this);
+        };
+        bcd();
+    },
+};
+d.write();//window
+
+//event handler context
+document.querySelector("h1").addEventListener("clcik",function()
+{
+    console.log("event handler",this);
+});//this value =html element
+
+//class context
+class person{
+    constructor(){
+        this.username = "aditi";
+        this.email = "aditi@gmail.com";
+        console.log("CLASS context",this);
+    }
+}
+let p1 = new person();
+console.log(p1);
+
+//context-->this keyword value
+//golbal context --> window
+//function context -->window
+//method context with es5 fnc -->object
+//method context with es6 fnc -->window
+// arrow function inside es5 fnc  -->object
+//es5 fnc inside es5 fnc --> window
+//event handler context -->html elment
+//class --> blank object
+
+//manual binding
+// class ,apply,bind
+//function ne call akrti vakhte this keyword ni value set karva mate call, apply,bind method no upyog kariye chiye
+
+
+let obj5 ={
+    name:"adii",
+    email:"adii@gmail.com",
+}//savethis obj.into this keyword
+
+function temp_a(){
+    console.log(this);
+}
+
+//function.call(object_name) --> this keyword ni value set karva mate call method no upyog kariye chiye 
+//temp.a.call(obj5);
+
+//function.apply(object_name)-->this keyword ni value set karva mate call method no upyog kariye chiye 
+//temp.a.apply(obj5);
+
+//function.bind(object_name)-->this keyword ni value set karva mate call method no upyog kariye chiye 
+let fnc =temp_a.bindf(obj5);//blank objeect create
+fnc();
+//console.log(fnc);
